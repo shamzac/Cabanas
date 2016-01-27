93b1e6e6a26d90c0df85591bb4ae7d623f0a36c2
Samuel <shamzac55@gmail.com>
2016-01-27 02:16:27 -0300

    Inicial

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		var barra = $(window).scrollTop();
+		var posicion =  (barra * 0.5);
+		
+		$('body').css({
+			'background-position': '0 +' + posicion + 'px'
+		});
+
+	});
+
+});
